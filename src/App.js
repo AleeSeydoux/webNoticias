2f002f0882ffd2a38a0409af96cb3f0f49286509
Ale <aleevelarde9@gmail.com>
2020-02-24 18:59:07 -0600

    Apikey agregada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Header from './componentes/Header';
 
 
 class App extends Component {
+
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    let url = 'http://newsapi.org/v2/top-headlines?country=mx&category=general&apiKey=008fd843d4f44c408ef43ee2858f13de';
+  }
+
   render(){
     return (
       <div className="App">
