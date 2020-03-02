7950f6e9ac7fbedb2653e8dea5297de93582eb33
Ale <aleevelarde9@gmail.com>
2020-03-02 14:52:01 -0600

    Noticias.js actualizado

--- src/componentes/Noticias.js
+++ src/componentes/Noticias.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Noticia from './Noticia';
 
 class Noticias extends Component {
     
     render() { 
-        return (  );
+        return ( 
+            <div className="row">
+                <Noticia />
+            </div>
+         );
     }
 }
  
