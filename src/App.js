e4b77ae5e484a43896986138ee6a10028a4d42bc
Ale <aleevelarde9@gmail.com>
2020-03-02 16:17:15 -0600

    Formulario importado en App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Noticias from './componentes/Noticias';
+import Formulario from './componentes/Formulario';
 
 
 class App extends Component {
@@ -35,6 +36,7 @@ class App extends Component {
         />
 
 <div className="container white contenedor-noticias">
+    <Formulario />
     <Noticias 
         noticias = {this.state.noticias}
       />
