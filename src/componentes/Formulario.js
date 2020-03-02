3069bfe821e1c5c21563c095e8398df4d724533e
Ale <aleevelarde9@gmail.com>
2020-03-02 16:17:43 -0600

    Formulario.js creado

---
+++ src/componentes/Formulario.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+    
+    render() { 
+        return ( <h1>
+            hola
+        </h1> 
+        );
+    }
+}
+ 
+export default Formulario;
